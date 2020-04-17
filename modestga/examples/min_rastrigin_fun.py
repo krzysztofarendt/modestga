427216bc75557b643d5e5135dd7ef6b312e0742f
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-04-17 22:45:52 +0200

    Update Rastrigin example

--- modestga/examples/min_rastrigin_fun.py
+++ modestga/examples/min_rastrigin_fun.py
@@ -7,11 +7,14 @@ from modestga import minimize
 logging.basicConfig(filename='ga.log', level='INFO', filemode='w')
 
 # Number of dimensions
-N = 2
+N = 50
 
 def fun(x, *args):
-    """Rastrigin function to be minimized"""
-    A = 10
+    """Rastrigin function to be minimized.
+
+    Global minimum y=0 at x=[0, 0, ..., 0].
+    """
+    A = 100
     global N
     y = A * N + np.sum(x ** 2 - A * np.cos(2 * np.pi * x))
     return y
@@ -40,11 +43,13 @@ fx_hist = list()
 args = (x_hist, fx_hist)
 
 options = {
-    'tol': 1e-6,
-    'pop_size': 300,
-    'trm_size': 30,
-    'mut_rate': 0.3,
-    'generations': 1000
+    'tol': 1e-8,
+    'pop_size': 200,
+    'trm_size': 40,
+    'mut_rate': 0.05,
+    'generations': 1000,
+    'inertia': 50,
+    'xover_ratio': 0.2
 }
 
 res = minimize(fun, bounds, args=args, callback=callback, options=options)
@@ -55,10 +60,10 @@ print(res)
 # Plot solution history
 
 fig, ax = plt.subplots(2, 1, sharex=True)
-ax[0].plot(x_hist)
+ax[0].plot(x_hist, alpha=0.1, color='k')
 ax[0].set_title('x')
-ax[1].plot(fx_hist)
-ax[1].set_title('f(x) = np.sum(x ** 2)')
+ax[1].plot(fx_hist, color='k')
+ax[1].set_title('f(x) = RASTRIGIN FUNC.')
 ax[1].set_xlabel('Generation')
 
 plt.show()
