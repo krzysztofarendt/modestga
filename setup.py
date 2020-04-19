a090bdf7a5149e42cc3315832a4be081b3339185
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-04-19 21:59:05 +0200

    v0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
+import setuptools
 
 setup(name='modestga',
-      version='0.2',
+      version='0.3',
       description='Genetic Algorithm minimization',
       url='https://github.com/krzysztofarendt/modestga',
       keywords='genetic algorithm optimization',
@@ -9,9 +10,7 @@ setup(name='modestga',
       author_email='krzysztof.arendt@gmail.com',
       license='BSD',
       platforms=['Windows', 'Linux'],
-      packages=[
-          'modestga'
-      ],
+      packages=setuptools.find_packages(),
       include_package_data=True,
       install_requires=[
           'numpy>=1.18.2',
