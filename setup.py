bf3a6f3c28836d36e5c0f753250faea75fb389e3
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2020-04-25 13:45:31 +0200

    Increase version to 0.5dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import setuptools
 
 setup(name='modestga',
-      version='0.4',
+      version='0.5dev',
       description='Genetic Algorithm minimization',
       url='https://github.com/krzysztofarendt/modestga',
       keywords='genetic algorithm optimization',
