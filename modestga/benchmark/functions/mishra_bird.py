cc34c4bf00382f5068a3574ec27e98e22f41f1f0
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2021-02-07 21:09:45 +0100

    Add Mishra's Bird example

---
+++ modestga/benchmark/functions/mishra_bird.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def mishra_bird(x, *args):
+    """Mishra's Bird constrained function with 2 parameters.
+
+    To be used in the constrained optimization examples.
+
+    When subject to:
+        (x[0] + 5) ** 2 + (x[1] + 5) ** 2 < 25
+    the global minimum is at f(-3.1302, -1.5821) = -106.7645
+
+    Bounds: -10 <= x[0] <= 0
+           -6.5 <= x[1] <= 0
+
+    Reference:
+    https://en.wikipedia.org/wiki/Test_functions_for_optimization
+    """
+    fx = np.sin(x[1]) * np.exp((1 - np.cos(x[0])) ** 2) + \
+         np.cos(x[0]) * np.exp((1 - np.sin(x[1])) ** 2) + (x[0] - x[1]) ** 2
+
+    return fx
+
+
+def mishra_bird_constr(x, *args):
+    """Constraint for the Mishra's Bird function."""
+    fx = (x[0] + 5) ** 2 + (x[1] + 5) ** 2 - 25
+
+    return fx * -1
---
+++ modestga/examples/con_min_mishra_fun.py
@@ -0,0 +1,30 @@
+"""Example of constrained optimization of the Mishra's Bird function.
+
+Global minimum at f(-4.1302, -1.5821) = -106.7645
+"""
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+from modestga import con_minimize
+from modestga.benchmark.functions.mishra_bird import mishra_bird
+from modestga.benchmark.functions.mishra_bird import mishra_bird_constr
+
+# Set up logging
+logging.basicConfig(
+    level='DEBUG',
+    filemode='w',
+    format="[%(processName)s][%(levelname)s] %(message)s"
+)
+
+# Run minimization
+res = con_minimize(
+    fun=mishra_bird,
+    bounds=[(-10., 0.), (-6.5, 0.)],
+    constr=[mishra_bird_constr],
+    workers=1,
+    options={
+        'tol': 1e-6
+    }
+)
+
+print(res)
