96733b81f852716ca5a7f12ca916b9bd334dbbd3
Krzysztof Arendt <krzysztof.arendt@gmail.com>
2021-02-07 21:13:49 +0100

    Remove stopping condition: fx < tol

--- modestga/ga.py
+++ modestga/ga.py
@@ -303,11 +303,6 @@ def minimize(fun, bounds, x0=None, args=(), callback=None, options={}, workers=N
             fx = fittest.val
             callback(x, fx, ng, *args)
 
-        # Break if successful, i.e. f(x) = 0
-        if fittest.val < opts['tol']:
-            exitmsg = "Solution found, f(x) < 0 + tol"
-            break
-
         # Break if stalled
         if nstalled >= opts['inertia']:
             exitmsg = \
