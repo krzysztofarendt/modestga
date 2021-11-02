9af254e0a00fa5291046ff069ab602cffe3ea058
krzysztofarendt <krzysztof.arendt@gmail.com>
2021-11-02 07:32:46 +0100

    Tidy up method analysis script

--- modestga/benchmark/methods_analyze_results.py
+++ modestga/benchmark/methods_analyze_results.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 def load_df(path):
     # Load data to a dataframe
     with open(path) as f:
-        d = yaml.load(f)
+        d = yaml.safe_load(f)
 
     df = pd.DataFrame(columns=["run", "ndim", "method", "f(x)", "nfev", "ng", "time"])
 
